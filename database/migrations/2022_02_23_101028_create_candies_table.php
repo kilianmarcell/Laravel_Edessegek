f9159044fe8b39c8f2c8614d52006905c6a834f6
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-23 11:22:53 +0100

    Candy tábla kész

--- database/migrations/2022_02_23_101028_create_candies_table.php
+++ database/migrations/2022_02_23_101028_create_candies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('candies', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->decimal("cocoa_content");
+            $table->decimal("sugar_content");
             $table->timestamps();
         });
     }
